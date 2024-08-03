2440bd190f0a2eddd083dff242934135bd413a1a
Dave Bryan <dave.tamakloe@gmail.com>
2024-08-03 05:21:55 -0700

    puppet_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
-# This Puppet manifest configures the SSH client to use a specific private key and disable password authentication.
+# This Puppet manifest configures the global SSH client to use a specific private key and disable password authentication.
 
-file { '/home/ubuntu/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   content => @("EOF")
-    Host your_server_alias
-        HostName 54.161.237.13
-        User ubuntu
+    Host *
         IdentityFile ~/.ssh/school
         PasswordAuthentication no
     | EOF
